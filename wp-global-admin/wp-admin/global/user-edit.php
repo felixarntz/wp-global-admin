ea3332106441633738583923245a5731e9cdda5b
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-02-12 01:55:32 +0100

    Fix global administrator setting when editing a user in global admin.

--- wp-global-admin/wp-admin/global/user-edit.php
+++ wp-global-admin/wp-admin/global/user-edit.php
@@ -13,23 +13,47 @@ if ( ! is_multinetwork() ) {
 	wp_die( __( 'Multinetwork support is not enabled.', 'wp-global-admin' ) );
 }
 
-// TODO: Form submission of this page does not work correctly.
-
+/**
+ * Handles changes to the global administrator privileges of a user.
+ *
+ * @since 1.0.0
+ * @access private
+ *
+ * @param int $user_id ID of the user that is being edited.
+ */
 function _ga_maybe_modify_global_administrator_setting( $user_id ) {
-	if ( is_multinetwork() && is_global_admin() && ! IS_PROFILE_PAGE && current_user_can( 'manage_global_options' ) && ! isset( $GLOBALS['global_administrators'] ) ) {
+	if ( is_multinetwork() && is_global_admin() && ! IS_PROFILE_PAGE && current_user_can( 'manage_global_options' ) ) {
 		$global_administrators = get_global_option( 'global_administrators', array() );
-		$user = get_userdata( $user_id );
+		$user                  = get_userdata( $user_id );
 
 		if ( ! empty( $_POST['global_administrator'] ) ) {
 			if ( ! in_array( $user->user_login, $global_administrators, true ) ) {
 				$global_administrators[] = $user->user_login;
 				update_global_option( 'global_administrators', $global_administrators );
+
+				/**
+				 * Fires after a user has been granted global administrator privileges.
+				 *
+				 * @since 1.0.0
+				 *
+				 * @param int $user_id User ID.
+				 */
+				do_action( 'granted_global_administrator', $user->ID );
 			}
 		} else {
 			$key = array_search( $user->user_login, $global_administrators, true );
 			if ( false !== $key ) {
 				array_splice( $global_administrators, $key, 1 );
 				update_global_option( 'global_administrators', $global_administrators );
+
+				/**
+				 * Fires after a user's global administrator privileges have been revoked.
+				 *
+				 * @since 1.0.0
+				 *
+				 * @param int $user_id User ID.
+				 */
+				do_action( 'revoked_global_administrator', $user->ID );
 			}
 		}
 	}
@@ -37,11 +61,31 @@ function _ga_maybe_modify_global_administrator_setting( $user_id ) {
 add_action( 'personal_options_update', '_ga_maybe_modify_global_administrator_setting', 10, 1 );
 add_action( 'edit_user_profile_update', '_ga_maybe_modify_global_administrator_setting', 10, 1 );
 
+/**
+ * Displays a checkbox to grant or revoke global administrator privileges.
+ *
+ * This function MUST be hooked into 'personal_options' as its HTML will only be valid there.
+ *
+ * @since 1.0.0
+ * @access private
+ *
+ * @param WP_User $user User that is being edited.
+ */
 function _ga_maybe_display_global_administrator_checkbox( $user ) {
-	if ( is_multinetwork() && is_global_admin() && ! IS_PROFILE_PAGE && current_user_can( 'manage_global_options' ) && ! isset( $GLOBALS['global_administrators'] ) ) {
+	if ( is_multinetwork() && is_global_admin() && ! IS_PROFILE_PAGE && current_user_can( 'manage_global_options' ) ) {
 		?>
+		</table>
+
+		<style type="text/css">
+			.user-role-wrap {
+				display: none;
+			}
+		</style>
+
+		<h2><?php _e( 'Permissions', 'wp-global-admin' ); ?></h2>
+
 		<table class="form-table">
-			<tr class="user-super-admin-wrap">
+			<tr class="user-global-admin-wrap">
 				<th>
 					<?php _e( 'Global Administrator', 'wp-global-admin' ); ?>
 				</th>
@@ -53,10 +97,9 @@ function _ga_maybe_display_global_administrator_checkbox( $user ) {
 					<?php endif; ?>
 				</td>
 			</tr>
-		</table>
 		<?php
 	}
 }
-add_action( 'personal_options', '_ga_maybe_display_global_administrator_checkbox', 10, 1 );
+add_action( 'personal_options', '_ga_maybe_display_global_administrator_checkbox', 9999, 1 );
 
 require( ABSPATH . 'wp-admin/user-edit.php' );
